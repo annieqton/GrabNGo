6d047de832f13a94256c8e0b4273b059eec2d688
Khalid Mohamud <kmohamud1@outlook.com>
2017-01-23 17:51:10 -0800

    finished day 1

---
+++ public/server.js
@@ -0,0 +1,26 @@
+'use strict';
+  const express = require('express'),
+  const requestProxy = require('express-request-proxy'),
+  const port = process.env.PORT || 3000,
+  const app = express();
+
+var proxyNYTimes = function(request, response) {
+  console.log('Routing NY times request for', request.params[0]);
+  (requestProxy({
+    url: 'https://api.nytimes.com/svc/topstories/v2/home.json' + request.params[0],
+    headers: { Authorization: 'token ' + process.env.NYT_TOKEN }
+  }))(request, response);
+};
+
+app.get('/nytimes/*', proxyNYTimes);
+
+app.use(express.static('./'));
+
+app.get('*', function(request, response) {
+  console.log('New request:', request.url);
+  response.sendFile('index.html', { root: '.' });
+});
+
+app.listen(port, function() {
+  console.log('Server started on port ' + port + '!');
+});
